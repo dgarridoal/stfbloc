0b3a7b74f97231d4a8f2bc18c13bfccc2c917a92
Diego Garrido <dgarridoal@gmail.com>
2023-10-24 21:20:27 -0300

    fixed all

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,8 +1,10 @@
-## 1.0.0 (2023-03-06)
+## <small>1.0.1 (2023-03-06)</small>
 
-* feat: :art: Refactor code ([8a20144](https://github.com/dgarridoal/stfbloc/commit/8a20144))
 * 📚 docs: Finish to mark done work ([a01578e](https://github.com/dgarridoal/stfbloc/commit/a01578e))
+* 1.0.1 ([d8a8ec7](https://github.com/dgarridoal/stfbloc/commit/d8a8ec7))
 * Initial proyect ([116d1b3](https://github.com/dgarridoal/stfbloc/commit/116d1b3))
+* chore: Add CHANGELOG.md ([ad241bf](https://github.com/dgarridoal/stfbloc/commit/ad241bf))
+* feat: :art: Refactor code ([8a20144](https://github.com/dgarridoal/stfbloc/commit/8a20144))
 
 
 
--- src/section/welcome.js
+++ src/section/welcome.js
@@ -1,14 +1,16 @@
-import { intro, outro } from '@clack/prompts'
-import color from 'picocolors'
-import { isFlutterProject } from '../flutter'
-export default function welcome () {
+import { intro, outro } from "@clack/prompts";
+import color from "picocolors";
+import { isFlutterProject } from "../flutter";
+export default function welcome() {
   intro(
     color.inverse(
-          ` Welcome to choose SState Manager for Flutter by ${color.cyan(' MrCajuka ')}`
+      ` Welcome to choose State Manager for Flutter by ${color.cyan(
+        " MrCajuka "
+      )}`
     )
-  )
+  );
   if (!isFlutterProject()) {
-    outro(color.red("Error: You aren't in a flutter project"))
-    return process.exit(1)
+    outro(color.red("Error: You aren't in a flutter project"));
+    return process.exit(1);
   }
 }
